699338b0b7557f966690a01cd2b90f2d6f8fcd14
louisqvo <louisqvo@gmail.com>
2018-10-22 16:27:20 -0400

    start a cuesheet and header

--- que/client/src/pages/CueSheet/CueSheet.js
+++ que/client/src/pages/CueSheet/CueSheet.js
@@ -1,45 +1,81 @@
-import React from 'react'
+import React, {Component} from 'react'
 import "./CueSheet.css"
 import NewHeader from "../NewHeader"
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {FormBtn, Input} from '../../components/Form'
 
-const CueSheet = ()=> (
-    <Router>
-        <div className="container">
-            <button className="btn btn-secondary float-right">
-                <a href="/newHeader" className="newHeaderBtn">
-                    New Header
-                </a>
-                <Route exact path="/newHeader" component={NewHeader}/>
-            </button>
-            <form>
-                <div className="form-group">
-                    <h2 className="text-md-left">Search Cue</h2>
-                    <input className="form-control"/>
+
+class CueSheet extends Component{
+    
+    state = {
+        search: ''
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log(this.state)
+        // if (this.state.title && this.state.author) {
+        //     API.saveBook({
+        //     title: this.state.title,
+        //     author: this.state.author,
+        //     synopsis: this.state.synopsis
+        //     })
+        //     .then(res => this.loadBooks())
+        //     .catch(err => console.log(err));
+        // }
+    };
+    
+    render(){
+        return(
+            <Router>
+                <div className="container">
+                    <button className="btn btn-secondary float-right">
+                        <a href="/newHeader" className="newHeaderBtn">
+                            New Header
+                        </a>
+                        <Route exact path="/newHeader" component={NewHeader}/>
+                    </button>
+                    <form>
+                        <h3>Search Cue</h3>
+                        <Input
+                            value={this.state.search}
+                            onChange={this.handleInputChange}
+                            name="search"
+                            placeholder="search"
+                        />
+                        <FormBtn
+                            onClick={this.handleFormSubmit}    
+                        >
+                            SUBMIT
+                        </FormBtn>
+
+                    </form>
+                
+                    <table className="table">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody className="cueInfo">
+                            {/* Cue info */}
+                        </tbody>
+                    </table>
                 </div>
-                <button type="submit" class="btn btn-secondary">Submit</button>
-            </form>
-
-
-            <table className="table">
-                <thead className="thead-dark">
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                    </tr>
-                    <tbody className="cueInfo">
-                        {/* Cue info */}
-                    </tbody>
-                </thead>
-            </table>
-        </div>
-    </Router>
-
-
-
-)
+            </Router>
+        )
+    }
+}
 
 export default CueSheet
