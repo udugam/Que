2d93accb586dc36fe74fbfa964b28446eaaead89
louisqvo <louisqvo@gmail.com>
2018-10-22 20:22:31 -0400

    delete some useless files

--- que/server.js
+++ que/server.js
@@ -1,20 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const app = express()
-const PORT = process.env.PORT || 3001
-
-const routes = require('./routes')
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-
 // Add routes, both API and view
 app.use(routes);
 
