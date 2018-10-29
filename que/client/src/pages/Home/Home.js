8783541278a60197a83534f2ebfd0ac2578a2313
rusomatroso <evgeny23kl@gmail.com>
2018-10-28 23:38:20 -0400

    Authentication added with test user

--- que/client/src/pages/Home/Home.js
+++ que/client/src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import AcrCloud from '../../components/ACRCloud/AcrCloud.js'
+import WelcomeUser from '../../components/WelcomeUser/WelcomeUser.js'
 
 const Home = ()=> (
     <div>
+        <WelcomeUser />
         <h1>This is the home page!</h1>
         <AcrCloud />
     </div>
