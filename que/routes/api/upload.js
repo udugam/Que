8584f8155069ffbc5527274193f63e503b0abdfd
Ryan Udugampola <ryan.udugam@gmail.com>
2018-11-08 17:47:41 +0000

    before pull from master

--- que/routes/api/upload.js
+++ que/routes/api/upload.js
@@ -5,19 +5,20 @@ router.route('/upload')
     .post(function(req,res) {
         let uploadedFile = req.files.audioFile;
         // Use the mv() method to place the file somewhere on your server
-        uploadedFile.mv('/Users/udugam/Documents/UofT-CodingBootcamp-Homework/NewQueFolder/Que/que/audio_files/uploadedFile.mp3', function(err) {
+        uploadedFile.mv('/root/Que/que/audio_files/uploadedFile.mp3', function(err) {
             if (err)
             return res.status(500).send(err);
 
             let options = {
                 mode: 'text',
-                pythonPath: '/usr/local/bin/python',
+                pythonPath: '/usr/bin/python',
                 pythonOptions: ['-u'], // get print results in real-time
-                //scriptPath: 'path/to/my/scripts',
-                args: ['-f','/Users/udugam/Documents/UofT-CodingBootcamp-Homework/NewQueFolder/Que/que/audio_files/uploadedFile.mp3']
+                scriptPath: '/root/Que/que/acrcloud_scan_files_python',
+                args: ['-f','/root/Que/que/audio_files/uploadedFile.mp3',
+			'-c', '/root/Que/que/acrcloud_scan_files_python/config.json']
             };
     
-            PythonShell.run(`/Users/udugam/Documents/UofT-CodingBootcamp-Homework/NewQueFolder/Que/que/acrcloud_scan_files_python/acrcloud_scan_files_python.py`, options, function(err) {
+            PythonShell.run('acrcloud_scan_files_python.py', options, function(err) {
                 if (err) throw err
                 console.log('finished')
             })
