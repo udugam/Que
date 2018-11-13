f16fb24d635379667fb55db46cab63c684387b6c
Ryan Udugampola <ryan.udugam@gmail.com>
2018-11-13 05:49:41 +0000

    fixed shareholdSongs type in models

--- que/server.js
+++ que/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-// var routes = require("./controllers/burgers_controller.js");
+// var routes = require("./controllers/burgers_controller.js";
 // Add routes, both API and view
 app.use(routes);
 
