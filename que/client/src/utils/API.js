ded39afc0a68e512049281ef03fa33daab9a97d9
udugam <ryan.udugam@gmail.com>
2018-12-06 00:03:27 -0500

    removed email variable from getsongs route in utils/API.js

--- que/client/src/utils/API.js
+++ que/client/src/utils/API.js
@@ -33,8 +33,8 @@ export default {
     editCue: function(cueData){
         return axios.post("/api/cues/edit", cueData)
     },
-    getSongs: function(email){
-        return axios.get("/api/shareholder/get/" + email)
+    getSongs: function(){
+        return axios.get("/api/shareholder/get/")
     },
     deleteCueSheet: function(id){
         console.log(id, "-------------")
